62f8c64a5b0fcf37ac6f492f9a7f8273fc107f06
Rohan Unbeg <rohanunbeg0918@gmail.com>
2025-04-13 14:46:11 +0530

    ''

--- api/server.js
+++ api/server.js
@@ -44,7 +44,7 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
