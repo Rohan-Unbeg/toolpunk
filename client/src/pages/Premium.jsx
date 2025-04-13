e4e3f457996dfcd6d6fa2740dd7c90830fb76137
Rohan Unbeg <rohanunbeg0918@gmail.com>
2025-04-13 18:58:52 +0530

    fix handle payments

--- client/src/pages/Premium.jsx
+++ client/src/pages/Premium.jsx
@@ -35,7 +35,7 @@ const Premium = () => {
             const amount = 100;
 
             const res = await fetch(
-                "https://toolpunk-api.onrender.com/api/create-order",
+                "https://toolpunk-api.onrender.com/api/create-order/",
                 {
                     mode: 'cors',
                     method: "POST",
