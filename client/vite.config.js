58e655a33992c9db33d89b5019a7cce5e8d47ee5
Rohan Unbeg <rohanunbeg0918@gmail.com>
2025-04-16 10:53:49 +0530

    updated cors

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,18 +5,18 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    // server: {
-    //   proxy: {
-    //     "/api": "https://toolpunk-api.onrender.com", // Proxy to your production backend
-    //   },
-    // },
     server: {
         proxy: {
-            "/api": {
-                target: "http://localhost:3000",
-                changeOrigin: true,
-                secure: false,
-            },
+            "/api": "https://toolpunk-api.onrender.com", // Proxy to your production backend
         },
     },
+    // server: {
+    //     proxy: {
+    //         "/api": {
+    //             target: "http://localhost:3000",
+    //             changeOrigin: true,
+    //             secure: false,
+    //         },
+    //     },
+    // },
 });
